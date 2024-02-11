cda29f04d372b53b2d814191bf398f555970f20c
ppdx999 <ppdx999@gmail.com>
2024-02-11 14:30:22 +0900

    example for <Translate> tag and docusaurus wirte-translations command

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import Translate from '@docusaurus/Translate';
 
 import styles from './index.module.css';
 
@@ -20,7 +21,12 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
+          <Translate
+            id="homepage.header.link"
+            description='The label of the homepage link to the docs'
+          >
             Docusaurus Tutorial - 5min ⏱️
+          </Translate>
           </Link>
         </div>
       </div>
