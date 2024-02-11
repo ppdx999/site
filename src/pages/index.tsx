4a2edcf343606affb45b7a087b4ca16fd1b9d24e
ppdx999 <ppdx999@gmail.com>
2024-02-11 17:41:27 +0900

    Update Top Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Link from '@docusaurus/Link';
 import Layout from '@theme/Layout';
 import Heading from '@theme/Heading';
 
@@ -19,6 +20,40 @@ function HomepageHeader() {
   );
 }
 
+function Card({title, link}) {
+  return (
+    <div className="card">
+      <Link to={link} className="card-body padding--lg">
+        <h3 className={clsx("card-title", styles.cardTitle)}>{title}</h3>
+      </Link>
+    </div>
+  );
+}
+
+const items = [
+  {title: 'Works', link: '/docs/intro'},
+  {title: 'Blog', link: '/blog'},
+  {title: 'Coming Soon', link: '/'},
+]
+
+function HomepageMain() {
+  return (
+    <main>
+      <section className="margin-vert--lg padding--lg">
+        <div className="container">
+          <div className="row">
+            {items.map((props, idx) => (
+              <div className="col col--4 margin-vert--md" key={idx}>
+                <Card {...props} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -26,6 +61,7 @@ export default function Home(): JSX.Element {
       title={siteConfig.title}
       description="a software developer">
       <HomepageHeader />
+      <HomepageMain />
     </Layout>
   );
 }
